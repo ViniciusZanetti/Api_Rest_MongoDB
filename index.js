f868314e83a702f2dfbb879271542e45c644733d
ViniciusZanetti <58536337+ViniciusZanetti@users.noreply.github.com>
2022-03-30 16:59:25 -0300

    projeto iniciado com rota teste

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
+
+app.get('/teste', (req, res) => {
+    res.send('Testando servidor!');
+})
